5ce195ba895552102c1e693d1d55624e7f413d2a
Michael <m.pluess@stud.unibas.ch>
2018-11-19 15:01:03 +0100

    TPR calculation is so quick, it feels like it works instantly (almost like the will of god, which shall strike the sinner in the exact moment of filthy pleasure)

--- src/java/processing/EfficiencyCalculator.java
+++ src/java/processing/EfficiencyCalculator.java
@@ -2,20 +2,23 @@ package processing;
 
 import data.Name;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class EfficiencyCalculator {
 
 	public static double calculateTruePositives(List<Name> cleanedNames, List<Name> correctNames) {
 		double numberOfCorrectNames = 0;
+		HashMap<String, Name> hashMap = new HashMap<>();
+
+		for (Name correctName : correctNames) {
+			hashMap.put(correctName.toString(), correctName);
+		}
 
 		// calculate number of names which appear in both lists
 		for (Name cleanedName : cleanedNames) {
-			for (Name correctName : correctNames) {
-				if (correctName.equals(cleanedName)) {
-					numberOfCorrectNames++;
-					break;
-				}
+			if (hashMap.containsKey(cleanedName.toString())) {
+				numberOfCorrectNames++;
 			}
 		}
 
